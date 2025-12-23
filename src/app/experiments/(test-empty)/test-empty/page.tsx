6be311b50df883a662d01016cac6ff0b936f8cb5
Razi Syed <syed.raziulhaque@gmail.com>
2025-12-23 16:36:50 -0500

    deleting test experiment

--- src/app/experiments/(test-empty)/test-empty/page.tsx
+++
@@ -1,9 +0,0 @@
-import TestEmpty from "@/components/experiments/test-empty/TestEmpty";
-
-export default function Page() {
-return (
-<div className="w-full h-screen bg-background">
-    <TestEmpty />
-</div>
-);
-}
--- src/components/experiments/test-empty/TestEmpty.test.tsx
+++
@@ -1,9 +0,0 @@
-import { expect, test } from 'vitest';
-import { render, screen } from '@testing-library/react';
-import TestEmpty from './TestEmpty';
-
-test('Test Empty renders correctly', () => {
-render(
-<TestEmpty />);
-expect(screen.getByText('Test Empty')).toBeDefined();
-});
