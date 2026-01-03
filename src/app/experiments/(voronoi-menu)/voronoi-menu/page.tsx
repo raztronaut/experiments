065fce74c961795e672ba4cf5ba5ae016d7021d9
Razi Syed <syed.raziulhaque@gmail.com>
2026-01-03 07:36:41 -0500

    revert: remove voronoi-menu experiment from repository (keep local only)

--- src/app/experiments/(voronoi-menu)/voronoi-menu/page.tsx
+++
@@ -1,9 +0,0 @@
-import VoronoiMenu from "@/components/experiments/voronoi-menu/VoronoiMenu";
-
-export default function Page() {
-return (
-<div className="w-full h-screen bg-white">
-    <VoronoiMenu />
-</div>
-);
-}
